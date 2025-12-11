7359b8a4200f69d897970a9f0721d61f2a5432b2
Sourav Sanjay <souravsanjay07@gmail.com>
2025-12-11 15:08:53 +0530

    Added the right click functionality

--- script.js
+++ script.js
@@ -13,7 +13,45 @@ files.addEventListener("click", function(evt) {
         // Add selection to the clicked file
         fileElement.classList.add("file-select");
         
-        console.log("Selected file:", fileElement.querySelector("h3").textContent);
+    }
+});
+
+let dock = document.querySelector(".dock");
+let tab = document.querySelector(".tab");
+let red = document.querySelector(".red");
+
+dock.addEventListener("click", function(evt){
+    if (tab.style.display === "block") {
+        tab.style.display = "none";
+    } else {
+        tab.style.display = "block";
+    }
+})
+
+red.addEventListener("click",()=>{
+    tab.style.display = "none";
+})
+
+let rightClickMenu = document.querySelector(".right-click-menu");
+let desktop = document.querySelector(".desktop");
+
+desktop.addEventListener("contextmenu", (e) => {
+    e.preventDefault(); // Prevent the default browser context menu
+    
+    // Position the menu where the user right-clicked
+    rightClickMenu.style.left = e.clientX + "px";
+    rightClickMenu.style.top = e.clientY + "px";
+    
+    // Show the menu
+    rightClickMenu.style.display = "flex";
+    
+});
+
+// Hide the right-click menu when clicking elsewhere
+desktop.addEventListener("click", (e) => {
+    // Don't hide if clicking on the menu itself
+    if (!rightClickMenu.contains(e.target)) {
+        rightClickMenu.style.display = "none";
     }
 });
 
